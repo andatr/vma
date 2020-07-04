891b7a3f8a5760ca5a97c9ee9c87e8991c810c49
Andatr <malcev.alexander@gmail.com>
2020-07-04 18:35:45 +0100

    vma/ directory was removed from inlcude path

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # vma
-Conan package for GPUOpen Vulkan Memory Allocator
+Conan package for [GPU Open Vulkan Memory Allocator](https://github.com/GPUOpen-LibrariesAndSDKs/VulkanMemoryAllocator)
+
+# How To Use
+
+1. Add the corresponding remote to conan:
+
+```bash
+conan remote add andatr https://api.bintray.com/conan/andatr/yaga
+```
+
+2. Add vma reference to `conanfile.txt`/`conanfile.py`:
+
+```
+[requires]
+vma/2.3@andatr/stable
+```
+
+3. Install dependencies for project:
+
+```bash
+conan install PROJECT_PATH --build=missing
+```
+
+4. Add following lines to CMakeLists.txt
+
+```cmake
+set(CMAKE_MODULE_PATH ${CMAKE_BINARY_DIR} ${CMAKE_MODULE_PATH})
+find_package(vma)
+```
+
+5. Add #include statement to C++ file
+
+```cpp
+#include <vk_mem_alloc.h>
+```
--- conanfile.py
+++ conanfile.py
@@ -7,23 +7,24 @@ class VmaConan(ConanFile):
     name = "vma"
     version = "2.3"
     license = "MIT"
-    description = "Vulkan Memory Allocator"
+    description = "Vulkan memory allocation library from GPUOpen"
     homepage = "https://github.com/GPUOpen-LibrariesAndSDKs/VulkanMemoryAllocator"
     topics = ("vma", "vulkan", "memory", "allocator")
-    exports_sources = "include/*"
+    exports_sources = "include/*", "licenses/*"
     no_copy_source = True
 
     def source(self):
         zip_name = "vma-2.3.0.zip"
-        dir_name = "include/vma"
         tools.download("https://codeload.github.com/GPUOpen-LibrariesAndSDKs/VulkanMemoryAllocator/zip/v2.3.0", zip_name)
         tools.check_md5(zip_name, "4EBE3957D75931D8945FC77A91EE4445")
-        os.makedirs(dir_name)
+        os.mkdir("include")
+        os.mkdir("licenses")
         tools.unzip(zip_name)
-        shutil.move("VulkanMemoryAllocator-2.3.0/src/vk_mem_alloc.h", dir_name)
+        shutil.move("VulkanMemoryAllocator-2.3.0/src/vk_mem_alloc.h", "include")
+        shutil.move("VulkanMemoryAllocator-2.3.0/LICENSE.txt", "licenses")
         shutil.rmtree("VulkanMemoryAllocator-2.3.0")
         os.unlink(zip_name)
 
     def package(self):
-        self.copy("*.h")
+        self.copy("*")
 
